dc7c38b6f948b3965e12b79bbc031644f1dc4a42
sohebakhter <sohebakhterbadhan@gmailcom>
2025-11-25 22:42:02 +0600

    added

--- app/products/page.js
+++ app/products/page.js
@@ -1,13 +1,29 @@
-import { allProducts } from "@/lib/getAllProducts";
+"use client";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
-export default async function ProductsPage() {
-  // Fetch products from DB or API with search filter
-  const products = await allProducts();
+export default function ProductsPage() {
+  const [search, setSearch] = useState("");
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://next-server-nu.vercel.app/products")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setProducts(data);
+      });
+  }, []);
+
+  const text = search.toLowerCase();
+
+  const filterSearche = products.filter((p) =>
+    p.title.toLowerCase().includes(text)
+  );
+  console.log(filterSearche);
 
   return (
     <div className="max-w-7xl mx-auto p-5">
-      {/* ========== PAGE TITLE ========== */}
       <div className="text-center mt-10 mb-14">
         <h1 className="text-4xl font-bold">Our Products</h1>
         <p className="text-gray-500 mt-2 text-lg">
@@ -16,27 +32,28 @@ export default async function ProductsPage() {
         </p>
       </div>
 
-      {/* ========== SEARCH + FILTER ========== */}
       <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-4 mb-10">
         {/* Search Bar */}
 
         <input
           type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
           placeholder="Search products..."
           className="input input-bordered w-full md:w-1/2"
         />
 
         {/* Category Filter (UI only) */}
-        {/* <select className="select select-bordered w-full md:w-48">
-          {categories.map((cat) => (
-            <option key={cat}>{cat}</option>
-          ))}
-        </select> */}
+        <select className="select select-bordered w-full md:w-48">
+          <option disabled={true}>Sort Here</option>
+          <option>Low--High</option>
+          <option>High--Low</option>
+        </select>
       </div>
 
       {/* ========== PRODUCTS GRID ========== */}
       <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8">
-        {products?.map((n) => (
+        {filterSearche?.map((n) => (
           <div
             key={n._id}
             className="card bg-base-100 shadow-md hover:shadow-xl hover:-translate-y-1 transition"
